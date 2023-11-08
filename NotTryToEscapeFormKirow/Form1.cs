6ac596c5ed0dcf8d3b25843bf498e763888aacf3
Arseni Luchinin <Lu4inin23@yandex.ru>
2023-11-08 21:11:13 +0300

    Создан класс Page, создано входное меню

--- NotTryToEscapeFormKirow/Form1.cs
+++ NotTryToEscapeFormKirow/Form1.cs
@@ -9,6 +9,36 @@ namespace NotTryToEscapeFormKirow
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MakeFullSkreen();
+
+            MainMenu = new Page(button1, button2, button3);
+            //MainMenu.Close();
+
+
+        }
+        private void MakeFullSkreen()
+        {
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainMenu.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MainMenu.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
 
         }
     }
---
+++ NotTryToEscapeFormKirow/Page.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotTryToEscapeFormKirow
+{
+    class Page
+    {
+        private Control[] Elements;
+        public Page(params Control[] arr)
+        {
+            Elements = arr;
+        }
+        public void Close() {
+            foreach (Control c in Elements) 
+                c.Visible = false;
+        }
+        public void Open()
+        {
+            foreach (Control c in Elements)
+                c.Visible = true;
+        }
+    }
+}
